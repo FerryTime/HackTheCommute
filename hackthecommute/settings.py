cfd49b15adda95293909f69a50154f894118a6bb
Qays H. Poonawala <qayshp@gmail.com>
2015-03-21 11:50:10 -0700

    Switch back to sqllite3 for dev.

--- hackthecommute/settings.py
+++ hackthecommute/settings.py
@@ -89,7 +89,8 @@ STATIC_URL = '/static/'
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config(default='postgres://djangouser:djangolocal@localhost:5433/django')
+if (os.environ['DATABASE_URL']):
+    DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
