6de81b8eb978b2285d1e03ad6319e7eb50eb1f51
Qays H. Poonawala <qayshp@gmail.com>
2015-03-21 17:31:39 -0700

    Added API call to get scheduled departures.

--- ferrytime/views.py
+++ ferrytime/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
+import json
+import requests
 
 # Create your views here.
 def index(request):
-    context = {}
+    schedule = ["hello", "world"]
+
+    url = "http://www.wsdot.wa.gov/Ferries/API/Schedule/rest/scheduletoday/6/false?apiaccesscode=d71bf03a-96aa-47cf-9fd1-25ec97e89d93"
+    terminal_combinations = requests.get(url).json().get(u'TerminalCombos')
+    kingston_departures= terminal_combinations[1]
+
+    terminal_id = kingston_departures.get('DepartingTerminalID')
+
+    times = kingston_departures.get(u'Times')
+
+    departures = list()
+    for time in times:
+        departures.append(time.get('DepartingTime'))
+
+    # need terminal ID and departure time
+    context = { "schedule":departures }
     return render(request, 'ferrytime/index.html', context)
 
 def forecast(request):
