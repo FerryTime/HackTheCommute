0f235521fb5d53e465ef1b79908c06344c3e40ca
kmusick314@gmail.com <kmusick314@gmail.com>
2015-03-21 17:55:54 -0700

    Forecast
    fixedbroken css and twitter

--- ferrytime/views.py
+++ ferrytime/views.py
@@ -10,18 +10,30 @@ def index(request):
     terminal_combinations = requests.get(url).json().get(u'TerminalCombos')
     kingston_departures= terminal_combinations[1]
 
-    terminal_id = kingston_departures.get('DepartingTerminalID')
+    terminal_id = kingston_departures.get(u'DepartingTerminalID')
 
     times = kingston_departures.get(u'Times')
 
     departures = list()
     for time in times:
-        departures.append(time.get('DepartingTime'))
+        departures.append(time.get(u'DepartingTime'))
 
     # need terminal ID and departure time
     context = { "schedule":departures }
     return render(request, 'ferrytime/index.html', context)
 
 def forecast(request):
-    context = {}
+    url = "http://www.wsdot.wa.gov/Ferries/API/Terminals/rest/terminalsailingspace?apiaccesscode=ffd8b8c2-c4e6-4784-a9df-2b8f7ee6dfc5"
+    terminal_combinations = requests.get(url).json().get(u'TerminalCombos')
+    kingston_departures= terminal_combinations[1]
+
+    terminal_id = kingston_departures.get(u'DepartingTerminalID')   
+    
+    data = kingston_departures.get(u'SpaceForArrivalTerminals')
+
+    driveUp = list()
+    for space in spaces:
+        driveUp.append(space.get(u'DriveUpSpaceCount')) 
+
+    context = {"space":driveUp}
     return render(request, 'ferrytime/forecast.html', context)
